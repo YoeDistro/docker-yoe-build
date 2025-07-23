02651d80551461ee887b189acb228914204e1eda
Khem Raj <raj.khem@gmail.com>
2025-07-22 20:51:20 -0700

    Add trixie (debian 13) based container
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -3,7 +3,7 @@ BRANCH=$(git branch --show-current)
 IMAGE=yoedistro/yoe-build:$BRANCH
 
 yoe_docker_build() {
-  DOCKER_BUILDKIT=1 docker build -t "$IMAGE" --platform linux/`uname -m` .
+  docker buildx build -t "$IMAGE" --platform linux/`uname -m` .
 }
 
 yoe_docker_upload() {
